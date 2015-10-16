de5ee4dbca2f0ce9a5f3605c6040b56faee9535a
yarisrespect <yarisrespect@gmail.com>
2015-10-16 18:37:37 +0300

    #

--- I18N.php
+++ I18N.php
@@ -1,14 +1,10 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Yaroslav
- * Date: 06.02.2015
- * Time: 12:55
- */
 
 namespace yarisrespect\i18n;
 
 use Yii;
+use yii\i18n\PhpMessageSource;
+use yii\log\Logger;
 
 class I18N extends \yii\i18n\I18N {
 
@@ -22,44 +18,65 @@ class I18N extends \yii\i18n\I18N {
 
     public function translate($category, $message, $params, $language)
     {
+
+        $result = parent::translate($category, $message, $params, $language);
+
         if($this->autoGenerate){
             $this->checkTranslate($category, $message, $language);
         }
 
-        return parent::translate($category, $message, $params, $language);
+        return $result;
     }
 
     public function checkTranslate($category, $message, $language){
-        // TODO: app -> app*
-        $messageSource = isset($this->translations[$category]) ? $this->translations[$category] : null ;
-
-        if($messageSource == null) return false;
-
-        $basePath = is_array($messageSource)?$messageSource['basePath']:$messageSource->basePath;
-        $sourceLanguage = is_array($messageSource)?(isset($messageSource['sourceLanguage'])?$messageSource['sourceLanguage']:null):$messageSource->sourceLanguage;
-        if($language != $sourceLanguage){
-            $baseDir = Yii::getAlias($basePath);
-            $langDir = $baseDir.DIRECTORY_SEPARATOR.$language;
-            $fileSrc = $langDir.DIRECTORY_SEPARATOR.$category.".php";
-
-            if( ! is_dir($baseDir) ) mkdir($baseDir, 0777);
-            if( ! is_dir($langDir) ) mkdir($langDir, 0777);
-
-            $translations = ! file_exists($fileSrc) ? [] : require( $fileSrc );
-            $translations = is_array($translations) ? $translations : [] ;
-
-            if( !isset($translations[$message]) ){
-                $message = str_replace('"', '\\"', $message);
-                $translations[$message] = $message;
-                $php_file = fopen($fileSrc, 'w');
-                fwrite($php_file, "<? \nreturn [ \n");
-                foreach($translations as $k => $v){
-                    fwrite($php_file, '     "'.$k.'"=>"'.$v.'",'."\n");
-                }
-                fwrite($php_file, "];");
-                fclose($php_file);
-            }
+        /** @var $messageSource PhpMessageSource */
+        $messageSource = $this->getMessageSource($category);
+        if(!($messageSource instanceof PhpMessageSource)){
+            return;
         }
 
+        $translation = $messageSource->translate($category, $message, $language);
+        if ($translation === false) {
+            $basePath = $messageSource->basePath;
+            $fileMap = $messageSource->fileMap;
+            if( isset($fileMap[$category]) ){
+                $file = $fileMap[$category];
+            } else {
+                $file = explode("/", $category)[0] . ".php";
+            }
+
+            $baseDir = Yii::getAlias($basePath);
+            $langDir = $baseDir.DIRECTORY_SEPARATOR.$language;
+            $fileSrc = $langDir.DIRECTORY_SEPARATOR.$file;
+
+            if( ! is_dir($baseDir) ) {
+                if(!mkdir($baseDir, 0777)) {
+                    Yii::getLogger()->log("yarisrespect/i18n, can`t create base directory {$basePath}!", Logger::LEVEL_WARNING);
+                    return;
+                }
+            }
+            if( ! is_dir($langDir) ) {
+                if(!mkdir($langDir, 0777)) {
+                    Yii::getLogger()->log("yarisrespect/i18n, can`t create language directory {$basePath}/{$language}!", Logger::LEVEL_WARNING);
+                    return;
+                }
+            }
+
+            if( !file_exists($fileSrc) ){
+                file_put_contents($fileSrc, "<? \nreturn [\n];");
+            }
+
+            $translations = file($fileSrc, FILE_SKIP_EMPTY_LINES);
+            $size = count($translations);
+            if( strpos("];", $translations[$size-1]) !== false){
+                $message = str_replace('"', '\\"', $message);
+                $translations[$size-1] = '     "'.$message.'"=>"'.$message.'",'."\n";
+                $translations[$size] = '];';
+                file_put_contents( $fileSrc, $translations );
+            } else {
+                Yii::getLogger()->log("yarisrespect/i18n, file {$file} - damaged!", Logger::LEVEL_WARNING);
+            }
+        }
     }
+
 } 
